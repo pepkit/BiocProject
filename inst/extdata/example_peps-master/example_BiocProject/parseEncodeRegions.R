d419d3e522c2d7f1f92a0ea8c2af049db5b594f7
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2018-10-23 15:39:34 -0400

    update examples

--- inst/extdata/example_peps-master/example_BiocProject/parseEncodeRegions.R
+++ inst/extdata/example_peps-master/example_BiocProject/parseEncodeRegions.R
@@ -7,13 +7,9 @@ parseEncodeRegions = function(project) {
   fileName = samples(project)$file_name[[1]]
   workDir = config(project)$metadata$output_dir
   # create the download dir if it does not exist
-  if(!dir.exists(file.path(workDir))){
-    message("\nCreating directory: ",workDir,"\n")
-    dir.create(file.path(workDir))
-  }
   # download the file
-  bfc = BiocFileCache(cache = workDir,ask = F)
-  path = bfcrpath(bfc,url)
+  bfc = BiocFileCache(cache = tempdir(),ask = FALSE)
+  path = bfcrpath(x = bfc, url)
   # read it in
   df=read.table(path)
   # formatting
