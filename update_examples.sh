6313dabd93947355d9049c2cdf5c4d00c4eb58b5
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-10-23 10:45:42 -0400

    enhance update_examples bash script

--- update_examples.sh
+++ update_examples.sh
@@ -1,5 +1,16 @@
-wget https://github.com/pepkit/example_peps/archive/master.zip
-unzip master.zip
-rm -rf inst/extdata/example_peps-master 
-mv example_peps-master inst/extdata
-rm master.zip
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo $0: usage: update_test_data.sh branch
+    exit 1
+fi
+
+branch=$1
+
+wget https://github.com/pepkit/example_peps/archive/${branch}.zip
+unzip ${branch}.zip
+rm -rf inst/extdata/example_peps-${branch} 
+mv example_peps-${branch} inst/extdata
+rm ${branch}.zip
+cd inst/extdata/example_peps-${branch}/
+find . -type d -maxdepth 1 ! -name 'example_BiocProjec*' ! -name 'example_piface' -exec rm -r {} \;
