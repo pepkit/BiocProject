ed30671470cb7882e8dc3c00cb605f4e401145d7
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-10-29 10:17:53 -0400

    use BiocFileCache to manage data for the tximeta vignette

--- long_vignettes/render-long-vignettes.R
+++ long_vignettes/render-long-vignettes.R
@@ -1,3 +1,4 @@
 knitr::opts_knit$set(base.dir = 'vignettes/', progress = TRUE, verbose = TRUE)
 knitr::opts_chunk$set(fig.path="vignette6tximeta/")
 knitr::knit("long_vignettes/vignette6tximeta.Rmd", "vignettes/vignette6tximeta.Rmd")
+
