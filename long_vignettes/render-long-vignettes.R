7fcd109cdd204bc16d56f30dac789a9ef267d1e2
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-10-29 14:44:11 -0400

    update tximeta vignette, fix paths

--- long_vignettes/render-long-vignettes.R
+++ long_vignettes/render-long-vignettes.R
@@ -1,4 +1,3 @@
 knitr::opts_knit$set(base.dir = 'vignettes/', progress = TRUE, verbose = TRUE)
 knitr::opts_chunk$set(fig.path="vignette6tximeta/")
 knitr::knit("long_vignettes/vignette6tximeta.Rmd", "vignettes/vignette6tximeta.Rmd")
-
